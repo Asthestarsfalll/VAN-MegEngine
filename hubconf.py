39c8824dfc49c0779f284c6252edc36bce870e1b
Asthestarsfalll <1186454801@qq.com>
2022-08-11 16:35:47 +0800

    fix import error

--- hubconf.py
+++ hubconf.py
@@ -1,8 +1,8 @@
 from models.van import (VAN, 
-                        VAN_b0, 
-                        VAN_b1, 
-                        VAN_b2, 
-                        VAN_b3, 
-                        VAN_b4, 
-                        VAN_b5, 
-                        VAN_b6)
+                        van_b0, 
+                        van_b1,
+                        van_b2,
+                        van_b3,
+                        van_b4,
+                        van_b5,
+                        van_b6)
